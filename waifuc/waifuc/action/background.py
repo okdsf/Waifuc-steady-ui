2ed49905c447c29a35b1d571377668d481fd0430
Monika <2425579813@qq.com>
2025-05-20 16:19:47 +0800

    Resive the bug of Removebackground in the current logic

--- waifuc/waifuc/action/background.py
+++ waifuc/waifuc/action/background.py
@@ -3,8 +3,23 @@ from imgutils.segment import segment_rgba_with_isnetis
 from .base import ProcessAction
 from ..model import ImageItem
 
-
 class BackgroundRemovalAction(ProcessAction):
-    def process(self, item: ImageItem) -> ImageItem:
-        _, image = segment_rgba_with_isnetis(item.image)
-        return ImageItem(image, item.meta)
+    def process(self, item):
+        if isinstance(item, ImageItem):
+            image = item.image
+            meta = item.meta
+        elif isinstance(item, Image.Image):
+            image = item.convert('RGB')
+            meta = {}
+        else:
+            raise TypeError(f"Expected ImageItem or PIL.Image, got {type(item)}")
+
+        _, rgba_image = segment_rgba_with_isnetis(image)
+        if rgba_image.mode == 'RGBA':
+            rgb_image = Image.new('RGB', rgba_image.size, (255, 255, 255))
+            rgb_image.paste(rgba_image, mask=rgba_image.split()[3])
+            image = rgb_image
+        else:
+            image = rgba_image
+
+        return ImageItem(image, meta)
